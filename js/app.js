5dec9ac6e5d08478aafd727b15f2cb42bb5d2009
InMemoriam7th <62008705+InMemoriam7th@users.noreply.github.com>
2020-05-11 11:55:13 +0200

    first commit

--- js/app.js
+++ js/app.js
@@ -11,16 +11,16 @@ $(document).ready(()=>{
             console.log(`${data.films[i].picture} ${data.films[i].nom} ${data.films[i].date} ${data.films[i].realisateur} ${data.films[i].genre} ${data.films[i].sortiedvd}`)
 
             $("ul").append(`<li>
-                            <figure>
-                            <img src="${data.films[i].picture}">
-                            <figcaption>
-                            <h2>${data.films[i].nom}</h2>
-                            <p> <a href="#">Date</a> ${data.films[i].date}</p>
-                            <p> <a href="#">Réal </a>${data.films[i].realisateur} </p>
-                            <p> <a href="#">Genre </a>${data.films[i].genre} </p>
-                            <p> <a href="#">Sortie dvd </a>${data.films[i].sortiedvd} </p>
-                            </figcaption>
-                            </figure>
+                              <figure>
+                                <img src="${data.films[i].picture}">
+                                  <figcaption>
+                                    <h2>${data.films[i].nom}</h2>
+                                    <p> <a href="#">Date</a> ${data.films[i].date}</p>
+                                    <p> <a href="#">Réal </a>${data.films[i].realisateur} </p>
+                                    <p> <a href="#">Genre </a>${data.films[i].genre} </p>
+                                    <p> <a href="#">Sortie dvd </a>${data.films[i].sortiedvd} </p>
+                                  </figcaption>
+                              </figure>
                             </li>
                             `)
         }
